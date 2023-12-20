f93b9b8fdfbcf133d6222440bd22b04d4af5aa12
Denis Artyushin <artyushinden@gmail.com>
2023-12-20 15:59:16 +0300

    Add timeout

--- github_graphql_client/client/requests_client.py
+++ github_graphql_client/client/requests_client.py
@@ -8,6 +8,13 @@ from .base import BaseGraphQLClient
 class RequestsClient(BaseGraphQLClient):
     """GraphQL client based on `requests` library."""
 
+    DEFAULT_TIMEOUT = 1
+
+    def __init__(self, endpoint: str, token: str, **kwargs: Any) -> None:
+        super().__init__(endpoint, token, **kwargs)
+
+        self.timeout = kwargs.get("timeout", RequestsClient.DEFAULT_TIMEOUT)
+
     def execute(
         self, query: str, variables: dict[str, Any], **kwargs: Any
     ) -> dict[str, Any]:
@@ -15,6 +22,7 @@ class RequestsClient(BaseGraphQLClient):
             url=self.endpoint,
             json={"query": query, "variables": variables},
             headers=self._get_header(),
+            timeout=self.timeout,
         )
         response = response.json()
         return response["data"]
