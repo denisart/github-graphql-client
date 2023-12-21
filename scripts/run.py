82d5abbc6409b6ec92b4ca3c0e778d9bdf3f6e9c
Denis Artyushin <artyushinden@gmail.com>
2023-12-21 12:58:23 +0300

    Update run file

--- scripts/run.py
+++ scripts/run.py
@@ -1,14 +1,7 @@
 import os
 import time
-from typing import Any
 
 from dotenv import load_dotenv
-
-from github_graphql_client.client.base import BaseGraphQLClient
-from github_graphql_client.client.requests_client import RequestsClient
-from github_graphql_client.queries.repository import (
-    get_repository_issues_query,
-)
 
 load_dotenv()  # take environment variables from .env
 
@@ -21,42 +14,14 @@ def check_execute(fn):
         tic = time.perf_counter()
         result = fn(*args, **kwargs)
         toc = time.perf_counter()
-        print(f"Duration is {toc - tic:0.4f} seconds")
+        print(f"Duration for {fn.__name__} is {toc - tic:0.4f} seconds")
         return result
 
     return wrapper
 
 
-@check_execute
-def execute_client(client: BaseGraphQLClient, **kwargs) -> Any:
-    return client.execute(**kwargs)
-
-
 def main():
-    client = RequestsClient(
-        endpoint=GITHUB_GRAPHQL_ENDPOINT, token=GITHUB_TOKEN
-    )
-
-    with client:
-        execute_client(
-            client,
-            query=get_repository_issues_query("pydantic", "FastUI"),
-            variables={},
-        )
-
-        execute_client(
-            client,
-            query=get_repository_issues_query("pydantic", "pydantic"),
-            variables={},
-        )
-
-        execute_client(
-            client,
-            query=get_repository_issues_query("pydantic", "pydantic-core"),
-            variables={},
-        )
-
-    print(f'client.session = {client.session}')
+    ...
 
 
 if __name__ == "__main__":
